ac5d5277969af3d401835115cb4815bea4a4c490
dementedchickens <123401219+dementedchickens@users.noreply.github.com>
2023-05-30 18:26:59 -0500

    Update ghostybae.js

--- Ghost Game/ghostybae.js
+++ Ghost Game/ghostybae.js
@@ -2,48 +2,49 @@ let ghost;
 let gImg;
 let cImg;
 let bImg;
-let candles=[];
-var score=0;
+let candles = [];
+var score = 0;
 
-
-function preload(){
-  gImg=loadImage("ghost normal.gif");
-  cImg=loadImage("candle1.gif");
-  bImg=loadImage("bancground.png");
-
+function preload() {
+  gImg = loadImage("ghost normal.gif");
+  cImg = loadImage("candle1.gif");
+  bImg = loadImage("background.png");
 }
-//Image not supported on Chrome :)
 
-function setup(){
+function setup() {
   createCanvas(600, 450);
-ghost= new Ghost();
+  ghost = new Ghost();
 }
 
-function keyPressed(){
-  if (key==' '){ ghost.jump();}
-}
-
-
-function draw()
-{
-if (random(1)<0.005){
-  candles.push(new Candle());
-   text(score, 400, 30);
-}
-function keyTyped(){if (key==" ")
-score=score+5;}
-
-  background(bImg);
-ghost.show();
-ghost.move();
- 
-for (let c of candles){
-  c.move();
-  c.show();
-  if(ghost.hits(c)){
-    console.log('game over');
-    noLoop();
+function keyPressed() {
+  if (key == ' ') {
+    ghost.jump();
   }
 }
+
+function draw() {
+  if (random(1) < 0.005) {
+    candles.push(new Candle());
+  }
+
+  background(bImg);
+  ghost.show();
+  ghost.move();
+
+  for (let c of candles) {
+    c.move();
+    c.show();
+    if (ghost.hits(c)) {
+      console.log('game over');
+      noLoop();
+    }
+  }
+
+  text(score, 400, 30);
 }
 
+function keyTyped() {
+  if (key == " ") {
+    score = score + 5;
+  }
+}
